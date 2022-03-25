b930895978a1a12788c60c8e5e9a7f12ce05da9e
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-25 19:37:44 +0300

    added photos for cards

--- src/components/app/cards.js
+++ src/components/app/cards.js
@@ -1,31 +1,35 @@
+import presto from '../../img/cards/presto.png';
+import solimo from '../../img/cards/Solimo.png';
+import aromistico from '../../img/cards/AromisticoMore.png';
+
 const cards = [
   {
-    id: 1, name: 'Solimo Coffee Beans 2 kg', country: 'Columbia', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 10.73, isBest: true,
+    id: 1, name: 'Solimo Coffee Beans 2 kg', country: 'Columbia', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: solimo, price: 10.73, isBest: true,
 
   },
   {
-    id: 2, name: 'Presto Coffee Beans 1 kg', country: 'Columbia', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 15.99, isBest: true,
+    id: 2, name: 'Presto Coffee Beans 1 kg', country: 'Columbia', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: presto, price: 15.99, isBest: true,
   },
   {
-    id: 3, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: true,
+    id: 3, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: true,
   },
   {
-    id: 4, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+    id: 4, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: false,
   },
   {
-    id: 5, name: 'AROMISTICO Coffee 1 kg', country: 'Kenya', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+    id: 5, name: 'AROMISTICO Coffee 1 kg', country: 'Kenya', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: false,
   },
   {
-    id: 6, name: 'AROMISTICO Coffee 1 kg', country: 'Columbia', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+    id: 6, name: 'AROMISTICO Coffee 1 kg', country: 'Columbia', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: false,
   },
   {
-    id: 7, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+    id: 7, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: false,
   },
   {
-    id: 8, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+    id: 8, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: false,
   },
   {
-    id: 9, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+    id: 9, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: aromistico, price: 6.99, isBest: false,
   },
 ];
 
