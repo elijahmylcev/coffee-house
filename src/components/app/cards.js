68068803f3f24054c2f8bd9da66fb6edf8ca0dc3
Elijah Mylcev <elijahmylcev@gmail.com>
2022-03-25 17:14:47 +0300

    Create cards information

---
+++ src/components/app/cards.js
@@ -0,0 +1,32 @@
+const cards = [
+  {
+    id: 1, name: 'Solimo Coffee Beans 2 kg', country: 'Columbia', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 10.73, isBest: true,
+
+  },
+  {
+    id: 2, name: 'Presto Coffee Beans 1 kg', country: 'Columbia', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 15.99, isBest: true,
+  },
+  {
+    id: 3, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: true,
+  },
+  {
+    id: 4, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+  },
+  {
+    id: 5, name: 'AROMISTICO Coffee 1 kg', country: 'Kenya', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+  },
+  {
+    id: 6, name: 'AROMISTICO Coffee 1 kg', country: 'Columbia', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+  },
+  {
+    id: 7, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+  },
+  {
+    id: 8, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+  },
+  {
+    id: 9, name: 'AROMISTICO Coffee 1 kg', country: 'Brazil', description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', img: '', price: 6.99, isBest: false,
+  },
+];
+
+export default cards;
